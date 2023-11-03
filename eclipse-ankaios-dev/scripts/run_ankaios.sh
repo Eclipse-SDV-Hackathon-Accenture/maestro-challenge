b2f9d280836a15561af8834f9f8df8955cbe84fc
lingnoi <chatree.akasarn@elektrobit.com>
2023-11-03 15:36:10 +0100

    added automation script to start workloads in order

--- eclipse-ankaios-dev/scripts/run_ankaios.sh
+++ eclipse-ankaios-dev/scripts/run_ankaios.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ANKAIOS_LOG_DIR="/var/log"
-mkdir -p ${ANKAIOS_LOG_DIR}
 
-# Start podman service
-pgrep podman &>/dev/null
-if [ $? -ne 0 ] 
-then
-    echo "Starting podman service"
-    podman system service --time=0 unix:///tmp/podman.sock &
-else 
-    echo "The podman service is already running."
-fi
-sleep 2
+echo '[Starting Ankaios cluster ...]'
+$SCRIPT_DIR/run_ankaios_default.sh &
 
-# Start the Ankaios server
-echo "Starting Ankaios server"
-ank-server --startup-config ${SCRIPT_DIR}/../config/startupState.yaml --address 127.0.0.1:25551 > ${ANKAIOS_LOG_DIR}/ankaios-server.log 2>&1 &
+sleep 3;
+while [ `ank -s http://127.0.0.1:25551 get state workloadStates |  yq '.workloadStates.[] | select(.workloadName == "hello") | .executionState' | grep -c ExecSucceeded` = 0 ]; do 
+  sleep 3; 
+done
 
-sleep 2
-# Start an Ankaios agent
-echo "Starting Ankaios agent agent_A"
-ank-agent --name agent_A -p /tmp/podman.sock --server-url http://127.0.0.1:25551 > ${ANKAIOS_LOG_DIR}/ankaios-agent_A.log 2>&1 &
+echo '[Ankaios cluster started.]'
 
-# Wait for any process to exit
-wait -n
+cat $SCRIPT_DIR/../config/startupState.yaml | yq '.workloads.* | path | .[-1]' | while read p; 
+do
+  yq ".workloads.$p | {\"currentState\": {\"workloads\": {\"$p\": .}}}"< $SCRIPT_DIR/../config/startupState.yaml > /tmp/currentState.yaml
+  echo "Starting workload '$p' ..."
+  ank -s http://127.0.0.1:25551 set state -f /tmp/currentState.yaml currentState.workloads.$p
+  while true; do
+    if [ $(ank -s http://127.0.0.1:25551 get state workloadStates |  yq ".workloadStates.[] | select(.workloadName == \"$p\") | .executionState" | grep -Ec '(ExecRunning|ExecFailed)') = 1 ]
+    then
+        break
+    fi
+    sleep 3; 
+  done
+  echo "Workload '$p' started."
+done 
 
-# Exit with status of process that exited first
-exit $?
+rm /tmp/currentState.yaml
+
