468339f97acc21156130802536e92e8035b9c2cf
Enrico Walther (IAV) <enrico.walther@iav.de>
2023-11-30 10:46:56 +0100

    Cleanup.

--- in-vehicle-stack/scenarios/smart_trailer_use_case/digital_twin_providers/common/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-// SPDX-License-Identifier: MIT
-
-pub mod constants;
-pub mod utils;
--- scenarios/wheelchair_assistant_use_case/start_wheelchair_assistant_provider_ankaios.sh
+++ scenarios/wheelchair_assistant_use_case/start_wheelchair_assistant_provider_ankaios.sh
@@ -6,14 +6,14 @@
 set -e
 
 # Deklariere das Array
-#PROVIDER_CONTAINERS=(
-#  "car_off_provider"
-#  "car_on_provider"
-#  "carkey_lock_provider"
-#  "carkey_unlock_provider"
-#  "wheelchair_distance_decreasing_provider"
-#  "wheelchair_distance_increasing_provider"
-#)
+PROVIDER_CONTAINERS=(
+  "car_off_provider"
+  "car_on_provider"
+  "carkey_lock_provider"
+  "carkey_unlock_provider"
+  "wheelchair_distance_decreasing_provider"
+  "wheelchair_distance_increasing_provider"
+)
 
 PROVIDER_CONTAINERS=(
   "car_off_provider"
@@ -26,5 +26,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Iterate over provider containers to build
 for CONTAINER in "${PROVIDER_CONTAINERS[@]}"; do
   CFG_PROVIDER="image: sdvblueprint.azurecr.io/sdvblueprint/in-vehicle-stack/--${CONTAINER}:0.1.0\ncommandOptions: [\"--network\", \"host\", \"--name\", \"${CONTAINER}\"]"
-   ank run workload ${CONTAINER} --runtime podman --config "$CFG_PROVIDER" --agent agent_A
+  echo "${CFG_PROVIDER}"
+  ank run workload ${CONTAINER} --runtime podman --config "$CFG_PROVIDER" --agent agent_A
 done
